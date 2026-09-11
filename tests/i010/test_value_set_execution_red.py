643e4f57f2792e09742e487c64fff3dfd42e0928
Oleksandr (Alex) Sosnovshchenko <alex.sosn88@gmail.com>
2026-09-11 20:07:01 +0300

    I-010 review: mark scalar projection executable

--- tests/i010/test_value_set_execution_red.py
+++ tests/i010/test_value_set_execution_red.py
@@ -158,6 +158,7 @@ def test_scalar_projection_cannot_execute_value_outside_authorized_dependencies(
     sources = noun_sources("bhsa", parent_char="a")
     projection = sources["mappings"]["mappings"][0]["projections"][0]
     projection["native_execution_binding"]["value"] = "verb"
+    projection["native_execution_binding"]["execution_shape"] = "value-predicate"
     _refresh_mapping(sources["mappings"]["mappings"][0])
     validate_structural_sources(sources)
     with pytest.raises(SemanticValidationError) as raised:
